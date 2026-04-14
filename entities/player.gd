c4c6ff7e79bd2af2512296fb3144d77c6dfa58c4
Xaklor <noqnfluxr@gmail.com>
2026-04-14 15:19:03 -0600

    basic 2D grid and movement

---
+++ entities/player.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+@onready var main = get_tree().get_root().get_node("main")
+@onready var tile_map: TileMapLayer = get_tree().get_root().get_node("main").get_node("tile_map")
+
+var grid_pos: Vector2i
+
+func _ready():
+	grid_pos = tile_map.local_to_map(position)
+
+func _input(event: InputEvent):
+	if event.is_action_pressed("move_down"):
+		if !tile_map.astar.is_point_solid(grid_pos + Vector2i(0, 1)):
+			position.y += tile_map.tile_set.tile_size.y
+			grid_pos.y += 1
+	if event.is_action_pressed("move_up"):
+		if !tile_map.astar.is_point_solid(grid_pos + Vector2i(0, -1)):
+			position.y -= tile_map.tile_set.tile_size.y
+			grid_pos.y -= 1
+	if event.is_action_pressed("move_left"):
+		if !tile_map.astar.is_point_solid(grid_pos + Vector2i(-1, 0)):
+			position.x -= tile_map.tile_set.tile_size.x
+			grid_pos.x -= 1
+	if event.is_action_pressed("move_right"):
+		if !tile_map.astar.is_point_solid(grid_pos + Vector2i(1, 0)):
+			position.x += tile_map.tile_set.tile_size.x
+			grid_pos.x += 1
---
+++ level/tile_map.gd
@@ -0,0 +1,18 @@
+extends TileMapLayer
+
+var astar = AStarGrid2D.new()
+
+func _ready():
+	var tilemap_size = get_used_rect().end - get_used_rect().position
+	astar.region = Rect2i(Vector2i.ZERO, tilemap_size)
+	astar.cell_size = tile_set.tile_size
+	astar.default_compute_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	astar.default_estimate_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	astar.update()
+	
+	for i in tilemap_size.x:
+		for j in tilemap_size.y:
+			var pos = Vector2i(i, j)
+			if get_cell_tile_data(pos).get_custom_data("tile_type") == "wall":
+				astar.set_point_solid(pos)
