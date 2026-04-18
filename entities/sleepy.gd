55d825f09efde156f5f675f1f0c0b97be8cc5583
Jason Wong <ja2415n@gmail.com>
2026-04-18 13:51:01 -0700

    add astar pathing to enemies

--- entities/sleepy.gd
+++ entities/sleepy.gd
@@ -3,19 +3,41 @@ extends Area2D
 @onready var main = get_tree().get_root().get_node("main")
 @onready var player = main.get_node("player")
 @onready var tile_map: TileMapLayer = get_tree().get_root().get_node("main").get_node("tile_map")
+@onready var wake_up_sound = $AudioStreamPlayer
+
+var asleep: bool = true
 
 # initialization
 func _ready():
 	player.ping.connect(on_player_ping)
-	
+
 # per-frame processing
 func _process(delta: float):
 	pass
 	
+func _input(event: InputEvent) -> void:
+	if (event.is_action_pressed("move_up") or
+	   	event.is_action_pressed("move_down") or
+	   	event.is_action_pressed("move_left") or
+	   	event.is_action_pressed("move_right")):
+		step()
+	
 # take a turn
 func step():
 	# sleepy is strong but passive until pinged
-	pass
+	if asleep:
+		return
+	
+	var curr_pos = tile_map.local_to_map(position)
+	var player_pos = tile_map.local_to_map(player.position)
+	
+	var path = tile_map.astar.get_id_path(curr_pos, player_pos)
+	if path.size() < 2:
+		return
+		
+	var next_pos = tile_map.map_to_local(path[1])
+	position = next_pos
+	
 
 # called when the player is in range and this enemy wants to attack them
 func intend_to_attack():
@@ -27,4 +49,5 @@ func take_damage(amount):
 	
 # called when the player uses a ping
 func on_player_ping():
-	pass
+	wake_up_sound.play()
+	asleep = false
--- entities/stabby.gd
+++ entities/stabby.gd
@@ -8,14 +8,29 @@ extends Area2D
 func _ready():
 	pass
 	
+	
 # per-frame processing
 func _process(delta: float):
 	pass
-	
+
+func _input(event: InputEvent) -> void:
+	if (event.is_action_pressed("move_up") or
+	   	event.is_action_pressed("move_down") or
+	   	event.is_action_pressed("move_left") or
+	   	event.is_action_pressed("move_right")):
+		step()
+
 # take a turn
 func step():
-	# stabby is weak but always aggressive
-	pass
+	var curr_pos = tile_map.local_to_map(position)
+	var player_pos = tile_map.local_to_map(player.position)
+	
+	var path = tile_map.astar.get_id_path(curr_pos, player_pos)
+	if path.size() < 2:
+		return
+		
+	var next_pos = tile_map.map_to_local(path[1])
+	position = next_pos
 
 # called when the player is in range and this enemy wants to attack them
 func intend_to_attack():
